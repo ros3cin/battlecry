7ecd72ff6e8dbe71a5ac4edf94e4db645d77cb40
Renato Oliveira dos Santos <rend.master@gmail.com>
2018-09-17 23:19:18 -0300

    Addition of compile dependency

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd src
-javac bcry/battlecry.java bcry/battlecryGUI.java bcry/battlecryLive.java
+javac -cp ../libs/commons-collections4-4.2.jar:../libs/eclipse-collections-9.2.0.jar:../libs/eclipse-collections-api-9.2.0.jar:. bcry/battlecry.java bcry/battlecryGUI.java bcry/battlecryLive.java
 mv bcry/*.class ../bcry
 cd ..
 ./makejar.sh
